b24ecb3066c9e5f73e80aa380c1ba2e02d4b9abb
Ian Chechin <Delnor40rus@gmail.com>
2024-03-18 16:33:42 +0300

    orm

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     
     class Status(models.TextChoices):
@@ -21,6 +26,8 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+    objects = models.Manager() #менеджер, применяемый по умолчанию
+    published = PublishedManager() #конкретно-прикладной менеджер
     
     class Meta:
         ordering = ['-publish']
