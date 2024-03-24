b61634849fa20876cf35db7caab36865fe236f68
Ian Chechin <Delnor40rus@gmail.com>
2024-03-24 16:58:06 +0300

    search engine

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from mysite.my_pass import EMAIL
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 
 
 # Create your views here.
@@ -99,3 +100,23 @@ def post_comment(request, post_id):
                             {'post': post,
                              'form': form,
                              'comment': comment})
+    
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + \
+                SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+
+    return render(request, 'blog/post/search.html',
+                {'form': form,
+                'query': query,
+                'results': results})
